8d8f29eb10ebb8cb3af4ef5c317af58394d368ff
Bruno Alassia <vruno.alassia@gmail.com>
2014-03-03 12:42:30 -0300

    render a list

--- render.js
+++ render.js
@@ -1,51 +1,65 @@
 try {
-	
+
 	jQuery.fn.render = function(source, data, config) {
-		
-		config = config || { append:false, prepend: false, async: false }
-		var response;
-		var el = this;
-		$.ajax({ 
-			url:source, 
-			data:[], 
-			success:function(response){
-				html = response;
-			}, 
-			async: false
-		});
-		
-		var replace = function(key, value, string) {
-			var reg = new RegExp("{%"+key+"}");
-			return string.replace(reg, value);
-		}
-		if(data instanceof Array) {
-			var len = data.length;
-			var aux;
-			for(var i=0; i< len; i++) {
-			
-			}
-		}else if(data instanceof Object) {
-			var aux = html;
-			for(key in data) {
-				aux = replace(key, data[key], aux);
-			}
-		}
-		html = aux;
+		try {
+			config = config || { append:false, prepend: false, html: true }
+			var response;
+			var el = this;
 
-		if(config.append) 
-		{
-			$(el).append(html);	
-		}
-		else if(config.prepend) 
-		{
-			$(el).prepend(html);	
-		}
-		else
-		{
-			$(el).html(html);	
-		}
+			// load html file to render  synchronously
+			$.ajax({ 
+				url:source, 
+				data:[], 
+				success:function(response){
+					html = response;
+				}, 
+				async: false
+			});
 
+			// create replace function
+			var replace = function(key, value, str) {
+				var reg = new RegExp("{%"+key+"}");
+				return str.replace(reg, value);
+			}
+
+			// render a list of object
+			if(data instanceof Array) {
+				var len = data.length;
+				var aux2= "";
+				console.log(data);
+				for(var i=0; i< len; i++) {
+					var aux = html;
+					var d = data[i];
+					for(key in d) {
+						aux = replace(key, d[key], aux);
+					}
+					aux2+=aux;
+				}
+				html = aux2;
+			}
+			// render a simple object
+			else if(data instanceof Object) {
+				var aux = html;
+				for(key in data) {
+					aux = replace(key, data[key], aux);
+				}
+				html = aux;
+			}
+
+			// html content can be render in 3 ways; 
+			// * append: true, append content at the end of the container, 
+			// * prepend: true, append content at the begining of the container,
+			// * html(default): true, replace the content of the container
+			if(config.append) { $(el).append(html);	 }
+			else if(config.prepend) { $(el).prepend(html); }
+			else { $(el).html(html); }
+
+		}catch(e) {
+			console.log(e);
+		}
 		return this;
 	}
-	
-}catch(e) {}
+
+}catch(e) {
+	console.log("jQuery is not defined, make sure to include the jquery library before including JQuery render extension");
+}
